b0ee17f467ec31136bc558bb097d2195cb230578
Oleg Shestakov <olshec@gmail.com>
2021-03-23 11:30:37 +0300

    fixed top function

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
 });
 
 // When the user clicks on the button, scroll to the top of the document
-/*function topFunction() {
+function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-}*/
+}
 
 function addEventListenerForCards() {
     let masCard = document.getElementsByClassName('card');
